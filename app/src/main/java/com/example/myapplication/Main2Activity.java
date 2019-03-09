a21c2611076f5b2ed97eaef42c50c8a8fb3a471f
Saikat Sengupta <saikatssg16@gmail.com>
2019-03-09 23:39:11 +0530

    added name field in MainActi

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
 
+    String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
